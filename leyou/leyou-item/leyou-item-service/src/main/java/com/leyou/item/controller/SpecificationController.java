5a1708916a9d061ae3a46da81ec30e37a0de015d
sc16s2w <765012395@qq.com>
2021-07-25 14:30:24 +0200

    add,update,delete.search finish for group and param

---
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/SpecificationController.java
@@ -0,0 +1,98 @@
+package com.leyou.item.controller;
+
+import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
+import com.leyou.item.service.SpecificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("spec")
+public class SpecificationController {
+    @Autowired
+    private SpecificationService specificationService;
+
+    /**
+     * 根据分类id查询参数组
+     * @param cid
+     * @return
+     */
+    @GetMapping("groups/{cid}")
+    public ResponseEntity<List<SpecGroup>> QuerySpecGroupByCid(@PathVariable("cid") Long cid){
+        List<SpecGroup> result = this.specificationService.QuerySpecGroupByCid(cid);
+        if(result == null|| result.size()<1){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("group/{id}")
+    public ResponseEntity<Void> DeleteGroupById(@PathVariable("id") Long id){
+        this.specificationService.DeleteGroupByid(id);
+        return ResponseEntity.noContent().build();
+    }
+    /**
+     * 添加group
+     * @param specGroup
+     * @return
+     */
+    @PostMapping("group")
+    public ResponseEntity<Void> AddSpecGroup(@RequestBody SpecGroup specGroup){
+        this.specificationService.AddSpecGroup(specGroup);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("group")
+    public ResponseEntity<Void> UpdateSpecGroup(@RequestBody SpecGroup specGroup){
+        this.specificationService.UpdateSpecGroup(specGroup);
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * 根据groupid查询params
+     * @param gid
+     * @return
+     */
+    @GetMapping("params")
+    public ResponseEntity<List<SpecParam>> QuertSpecParamByQid(@RequestParam("gid") Long gid){
+        List<SpecParam> result = this.specificationService.QuerySpecParamByQid(gid);
+        if(result == null || result.size()<1){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+
+
+    /**
+     * 增加参数
+     */
+    @PostMapping("param")
+    public ResponseEntity<Void> AddSpecParam(@RequestBody SpecParam specParam){
+        this.specificationService.AddSpecParam(specParam);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
+
+    /**
+     * 更新参数
+     */
+    @PutMapping("param")
+    public ResponseEntity<Void> UpdateSpecParam(@RequestBody SpecParam specParam){
+        this.specificationService.UpdateSpecParam(specParam);
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * 删除参数
+     */
+    @DeleteMapping("param/{id}")
+    public ResponseEntity<Void> DeleteSpecParamById(@PathVariable("id") Long id){
+        this.specificationService.DeleteSpecParamById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -0,0 +1,90 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.mapper.SpecParamMapper;
+import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpecificationService {
+    @Autowired
+    private SpecGroupMapper specGroupMapper;
+
+    @Autowired
+    private SpecParamMapper specParamMapper;
+
+    /**
+     * 查询group
+     * @param cid
+     * @return
+     */
+    public List<SpecGroup> QuerySpecGroupByCid(Long cid) {
+        SpecGroup record = new SpecGroup();
+        record.setCid(cid);
+        return this.specGroupMapper.select(record);
+    }
+
+    /**
+     * 查询param
+     * @param gid
+     * @return
+     */
+    public List<SpecParam> QuerySpecParamByQid(Long gid){
+        SpecParam specParam = new SpecParam();
+        specParam.setGroupId(gid);
+        return this.specParamMapper.select(specParam);
+    }
+
+    /**
+     * 增加group
+     * @param specGroup
+     */
+    public void AddSpecGroup(SpecGroup specGroup) {
+        this.specGroupMapper.insert(specGroup);
+    }
+
+    /**
+     * 更新group
+     * @param specGroup
+     */
+    public void UpdateSpecGroup(SpecGroup specGroup) {
+        this.specGroupMapper.updateByPrimaryKey(specGroup);
+
+    }
+
+    /**
+     * 根据id删除group
+     * @param id
+     */
+    public void DeleteGroupByid(Long id) {
+        this.specGroupMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 增加参数
+     * @param specParam
+     */
+    public void AddSpecParam(SpecParam specParam) {
+        this.specParamMapper.insert(specParam);
+    }
+
+    /**
+     * 更新参数
+     * @param specParam
+     */
+    public void UpdateSpecParam(SpecParam specParam) {
+        this.specParamMapper.updateByPrimaryKey(specParam);
+    }
+
+    /**
+     * 删除参数
+     * @param id
+     */
+    public void DeleteSpecParamById(Long id) {
+        this.specParamMapper.deleteByPrimaryKey(id);
+    }
+}

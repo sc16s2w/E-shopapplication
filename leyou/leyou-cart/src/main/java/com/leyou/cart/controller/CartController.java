4048760ffcc911e83887b6ec865589b8452186e0
sc16s2w <765012395@qq.com>
2021-08-07 20:00:54 +0200

    order service finish

--- leyou/leyou-cart/src/main/java/com/leyou/cart/controller/CartController.java
+++ leyou/leyou-cart/src/main/java/com/leyou/cart/controller/CartController.java
@@ -5,9 +5,9 @@ import com.leyou.cart.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class CartController {
@@ -24,6 +24,36 @@ public class CartController {
     public ResponseEntity<Void> addCart(@RequestBody Cart cart){
         this.cartService.addCart(cart);
         return ResponseEntity.ok().build();
+    }
 
+    /**
+     * 查询购物车里的元素
+     * @return
+     */
+    @GetMapping
+    public ResponseEntity<List<Cart>> queryCarts(){
+        List<Cart> carts = this.cartService.queryCarts();
+        if(carts == null||carts.size()<1){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(carts);
+    }
+
+
+    /**
+     * 更新物品数量
+     * @param cart
+     * @return
+     */
+    @PutMapping
+    public ResponseEntity<Void> changecarts(@RequestBody Cart cart){
+        this.cartService.changeCarts(cart);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{skuId}")
+    public ResponseEntity<Void> deleteCart(@PathVariable("skuId")String skuId){
+        this.cartService.deleteCart(skuId);
+        return ResponseEntity.noContent().build();
     }
 }
--- leyou/leyou-cart/src/main/java/com/leyou/cart/service/CartService.java
+++ leyou/leyou-cart/src/main/java/com/leyou/cart/service/CartService.java
@@ -11,6 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CartService {
@@ -53,5 +57,50 @@ public class CartService {
         }
         hashOperations.put(skuId.toString(),JsonUtils.serialize(cart));
         //不在的话新增数量
+    }
+
+    /**
+     * 查询购物车里的元素
+     * @return
+     */
+    public List<Cart> queryCarts() {
+        UserInfo userInfo = LoginInterceptor.getLoginUser();         //查询购物车记录
+        if(!this.redisTemplate.hasKey(KEY_PREFIX+userInfo.getId())){
+            return null;
+        }
+        BoundHashOperations<String,Object,Object> hashOperations = this.redisTemplate.boundHashOps(KEY_PREFIX+userInfo.getId());
+        //获取map中所有的cart的值
+        List<Object> cartsJson = hashOperations.values();
+        if(CollectionUtils.isEmpty(cartsJson)){
+            return null;
+        }
+        //把list<object>集合转为list<cart>集合
+        return cartsJson.stream().map(cartJson->JsonUtils.parse(cartJson.toString(),Cart.class)).collect(Collectors.toList());
+    }
+
+    /**
+     * 更新物品数量
+     * @param cart
+     */
+    public void changeCarts(Cart cart) {
+        UserInfo userInfo = LoginInterceptor.getLoginUser();
+        if(!this.redisTemplate.hasKey(KEY_PREFIX+userInfo.getId())){
+            return;
+        }
+        BoundHashOperations<String,Object,Object> hashOperations = this.redisTemplate.boundHashOps(KEY_PREFIX+userInfo.getId());
+        String cartJson = hashOperations.get(cart.getSkuId().toString()).toString();
+        Cart cart1 = JsonUtils.parse(cartJson, Cart.class);
+        cart1.setNum(cart.getNum());
+        hashOperations.put(cart.getSkuId().toString(),JsonUtils.serialize(cart1));
+    }
+
+    /**
+     * 删除数量
+     * @param skuId
+     */
+    public void deleteCart(String skuId){
+        UserInfo userInfo = LoginInterceptor.getLoginUser();
+        BoundHashOperations<String,Object,Object> hashOperations = this.redisTemplate.boundHashOps(KEY_PREFIX+userInfo.getId());
+        hashOperations.delete(skuId);
     }
 }
